31ea0a81e0dacc682c1993102a3ff24c4018cc7c
git-brainscott <157086007+git-brainscott@users.noreply.github.com>
2025-03-05 16:06:07 -0800

    hi

---
+++ send_email.php
@@ -0,0 +1,30 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Get form data
+    $name = htmlspecialchars($_POST['name']);
+    $email = htmlspecialchars($_POST['email']);
+    $subject = htmlspecialchars($_POST['subject']);
+    $message = htmlspecialchars($_POST['message']);
+    
+    // Email settings
+    $to = "brianiscoolieo957@gmail.com"; // Replace with your email
+    $subject = "New Contact Form Submission: $subject";
+    $headers = "From: $email" . "\r\n" .
+               "Reply-To: $email" . "\r\n" .
+               "X-Mailer: PHP/" . phpversion();
+
+    // Prepare the email body
+    $email_body = "You have received a new message from your website contact form.\n\n";
+    $email_body .= "Name: $name\n";
+    $email_body .= "Email: $email\n";
+    $email_body .= "Subject: $subject\n";
+    $email_body .= "Message:\n$message\n";
+
+    // Send the email
+    if (mail($to, $subject, $email_body, $headers)) {
+        echo "Thank you for your message. It has been sent.";
+    } else {
+        echo "Sorry, something went wrong. Please try again later.";
+    }
+}
+?>
